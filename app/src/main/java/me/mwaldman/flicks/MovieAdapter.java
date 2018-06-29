da01f7deaf431d8b37446029602a724c95e0dc57
Michael Waldman <mwaldman@fb.com>
2018-06-29 14:46:03 -0700

    Working except for displaying images

--- app/src/main/java/me/mwaldman/flicks/MovieAdapter.java
+++ app/src/main/java/me/mwaldman/flicks/MovieAdapter.java
@@ -1,6 +1,7 @@
 package me.mwaldman.flicks;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
@@ -76,7 +79,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     //create the viewholder as a static inner class
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         ImageView ivPosterImage;
         ImageView ivBackdropImage;
         TextView tvTitle;
@@ -87,7 +90,24 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             ivBackdropImage = (ImageView) itemView.findViewById(R.id.ivBackdropImage);
             tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
             tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+            itemView.setOnClickListener(this);
+        }
 
+        @Override
+        public void onClick(View view) {
+        // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
     }
 }
--- app/src/main/java/me/mwaldman/flicks/models/Movie.java
+++ app/src/main/java/me/mwaldman/flicks/models/Movie.java
@@ -2,20 +2,31 @@ package me.mwaldman.flicks.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class Movie {
     //values from API
-    private String title;
-    private String overview;
-    private String posterPath;//only the path
+     String title;
+     String overview;
+     String posterPath;//only the path
     //initialize from JSON data
-    private String backdropPath;
+     String backdropPath;
+     Double voteAverage;
+     public Movie(){
+
+     }
     public Movie(JSONObject object) throws JSONException {
         title = object.getString("title");
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+        voteAverage = object.getDouble("vote_average");
 
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 
     public String getTitle() {
